7c99ba6c12cdf9cb219614b7ed29bc420e8c11bc
Fitsum Tsehay <tsehayfitsum@gmail.com>
2022-10-06 01:28:28 -0400

    Performed CRUD operations on Product List

---
+++ src/main/java/com/product/list/ProductlistApplication.java
@@ -0,0 +1,13 @@
+package com.product.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/product/list/ProductlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
