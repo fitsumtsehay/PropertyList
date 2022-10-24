a5531084947a756b65146802a0c0d4ed9d884b4c
Fitsum Tsehay <tsehayfitsum@gmail.com>
2022-10-24 19:29:36 -0400

    Executed CRUD Operation

--- src/main/java/com/product/list/Product.java
+++ src/main/java/com/product/list/Product.java
@@ -7,48 +7,57 @@ import javax.persistence.Id;
 
 @Entity
 public class Product {
+
     private Long id;
-    private String name;
-    private String brand;
-    private String madein;
+    private String propertyType;
+    private String propertyAddress;
+    private String numberOfBedrooms;
     private float price;
 
     protected Product() {
     }
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
-    public String getName() {
-        return name;
+
+    public String getPropertyType() {
+        return propertyType;
     }
-    public void setName(String name) {
-        this.name = name;
+
+    public void setPropertyType(String propertyType) {
+        this.propertyType = propertyType;
     }
-    public String getBrand() {
-        return brand;
+
+    public String getPropertyAddress() {
+        return propertyAddress;
     }
-    public void setBrand(String brand) {
-        this.brand = brand;
+
+    public void setPropertyAddress(String propertyAddress) {
+        this.propertyAddress = propertyAddress;
     }
-    public String getMadein() {
-        return madein;
+
+    public String getNumberOfBedrooms() {
+        return numberOfBedrooms;
     }
-    public void setMadein(String madein) {
-        this.madein = madein;
+
+    public void setNumberOfBedrooms(String numberOfBedrooms) {
+        this.numberOfBedrooms = numberOfBedrooms;
     }
+
     public float getPrice() {
         return price;
     }
+
     public void setPrice(float price) {
         this.price = price;
     }
-
-
-
 }
+
+
